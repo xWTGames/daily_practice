5ec619ec03ae95f139111e8793f3989f14a271cf
xWTGames <vadim.galyas@gmail.com>
2025-08-07 00:46:37 +0300

    daily practice

--- PythonProject/task_tracker.py
+++ PythonProject/task_tracker.py
@@ -1,8 +1,9 @@
 task_list = []
 
 def show_menu():
-    print(f"Виберіть опцію: \n 1. Додати завдання. \n 2. Показати список завдань. \n 3. Оновити завдання. \n 4. Видалити завдання. \n 5. Змінити статус завдання.")
+    print(f"Виберіть опцію: \n 1. Додати завдання. \n 2. Показати список завдань. \n 3. Оновити завдання. \n 4. Видалити завдання. \n 5. Змінити статус завдання. \n 6. Показати завдання тільки в конкретному статусі.")
     return input("Введіть номер опції: ")
+
 
 def add_task():
     task_text = input("Запишіть завдання: ")
@@ -12,10 +13,12 @@ def add_task():
     task_list.append(task)
     print("Завдання записано!")
 
+
 def show_tasks():
     print("Список завдань: ")
     for t in task_list:
         print(f"{t["id"]}. {t["text"]} - {t["status"]}")
+
 
 def get_task_index():
     while True:
@@ -29,31 +32,32 @@ def get_task_index():
             print("Невірне значення, спробуйте ще раз.")
 
 
-
 def edit_task():
     show_tasks()
     choice = get_task_index()
     task_text = input("Запишіть оновлене завдання: ")
-    task = {"id": choice,
-            "text": task_text,
-            "status": "не виконано"}
-    task_list[choice] = task
+    task_list[choice]["text"] = task_text
 
 
 def del_task():
     show_tasks()
     choice = get_task_index()
     task_list.pop(choice)
+
+    for i, task in enumerate(task_list, start=1):
+        task["id"] = i
+
     print("Завдання успішно видалено!")
+
 
 def mark_task():
     show_tasks()
     choice = get_task_index()
 
     statuses = {
-        "1": "Виконано",
-        "2": "В процесі",
-        "3": "Не виконано"
+        "1": "виконано",
+        "2": "в процесі",
+        "3": "не виконано"
     }
     while True:
         print("Виберіть, який статус призначити: \n 1. Виконано \n 2. В процесі \n 3. Не виконано")
@@ -67,20 +71,24 @@ def mark_task():
 
 
 def specific_list():
-    choice = input(f"Виберіть, який список завдань вивести: \n 1. Виконані \n 2. В процесі \n 3. Не виконані")
-    if choice == "1":
-        for t in task_list:
-            if t["status"] == "Виконано":
-                print(f"{t["id"]}. {t["text"]} - {t["status"]}")
-    elif choice == "2":
-        for t in task_list:
-            if t["status"] == "В процесі":
-                print(f"{t["id"]}. {t["text"]} - {t["status"]}")
-    elif choice == "3":
-        for t in task_list:
-            if t["status"] == "Не виконано":
-                print(f"{t["id"]}. {t["text"]} - {t["status"]}")
+    while True:
+        choice = input(
+            f"Виберіть, який список завдань вивести: \n 1. Виконані \n 2. В процесі \n 3. Не виконані\nВаш вибір: "
+        )
+        statuses = {
+            "1": "виконано",
+            "2": "в процесі",
+            "3": "не виконано"
+        }
 
+        if choice not in statuses:
+            print("Невірне значення, спробуйте ще раз.")
+            continue
+
+        for t in task_list:
+            if t["status"] == statuses[choice]:
+                print(f"{t['id']}. {t['text']} - {t["status"]}")
+        break
 
 
 while True:
@@ -88,16 +96,21 @@ while True:
     if option == "1":
         add_task()
 
-    if option == "2":
+    elif option == "2":
         show_tasks()
 
-    if option == "3":
+    elif option == "3":
         edit_task()
 
-    if option == "4":
+    elif option == "4":
         del_task()
 
-    if option == "5":
+    elif option == "5":
         mark_task()
+
+    elif option == "6":
+        specific_list()
+
+
 
 
